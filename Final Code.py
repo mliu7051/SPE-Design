72cc624db1c1c58ca9c6ec454cd8605558463446
Conrad Clement <oconradh@gmail.com>
2020-07-17 16:24:08 -0600

    fix columns, add lasso/ridge, start gridsearch

--- Final Code.py
+++ Final Code.py
@@ -10,7 +10,10 @@ from sklearn.ensemble import RandomForestRegressor as rfr
 from sklearn.tree import DecisionTreeRegressor as dtr
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression as lr
+from sklearn.linear_model import Lasso as lasso
+from sklearn.linear_model import Ridge as ridge
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
 from sklearn import svm
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -24,14 +27,14 @@ import matplotlib.pyplot as plt
 #for each model by comparing each model's predicted values of conductivity
 #against the actual conductivity values.
 
-
-literature_data = pd.read_csv('Feed In Data.csv')
+# Add skiprows b/c it was taking data set names to be column titles
+literature_data = pd.read_csv('Feed In Data.csv', skiprows=1)
 
 #Dataset 1 - this line extracts dataset 1 from the complete dataset file
-literature_data = literature_data.drop(columns=['1author3', '1author2', 'LiTFSIwt2', 'temp C2', 'exponent2', 'Unnamed: 4', 'LiTFSIwt', 'temp C', 'exponent', 'Unnamed: 9', '1author'])
+#literature_data = literature_data.drop(columns=['1author3', '1author2', 'LiTFSIwt2', 'temp C2', 'exponent2', 'Unnamed: 4', 'LiTFSIwt', 'temp C', 'exponent', 'Unnamed: 9', '1author'])
 
 #Dataset 2 - this line extracts dataset 2 from the complete dataset file
-literature_data = literature_data.drop(columns=['1author2', '1author3', 'LiTFSIwt3', 'temp C3', 'exponent3', 'Unnamed: 4', 'LiTFSIwt', 'temp C', 'exponent', 'Unnamed: 9', '1author'])
+#literature_data = literature_data.drop(columns=['1author2', '1author3', 'LiTFSIwt3', 'temp C3', 'exponent3', 'Unnamed: 4', 'LiTFSIwt', 'temp C', 'exponent', 'Unnamed: 9', '1author'])
 
 #Dataset 3 - this line extracts dataset 3 from the complete dataset file
 literature_data = literature_data.drop(columns=['1author2', '1author3', 'LiTFSIwt3', 'temp C3', 'exponent3', 'Unnamed: 4', 'LiTFSIwt2', 'temp C2', 'exponent2', 'Unnamed: 9', '1author'])
@@ -52,6 +55,29 @@ linreg_pred = linreg.predict(X_test)
 linreg_rmse = mean_squared_error(y_test, linreg_pred)
 print('linreg MAE: ' + str(sum(abs(linreg_pred - y_test))/(len(y_test))))
 print('linreg RMSE: ' + str(np.sqrt(linreg_rmse)))
+
+
+#-------------------------------lasso regression train and test------------------------------------------------------------------
+
+lassoreg = lasso(normalize=True)
+lassoreg.fit(X_train, y_train)
+lassoreg_pred = lassoreg.predict(X_test)
+
+lassoreg_rmse = mean_squared_error(y_test, lassoreg_pred)
+print('lassoreg MAE: ' + str(sum(abs(lassoreg_pred - y_test))/(len(y_test))))
+print('lassoreg RMSE: ' + str(np.sqrt(lassoreg_rmse)))
+
+
+#-------------------------------ridge regression train and test------------------------------------------------------------------
+
+ridgereg = ridge(normalize=True)
+ridgereg.fit(X_train, y_train)
+ridgereg_pred = ridgereg.predict(X_test)
+
+ridgereg_rmse = mean_squared_error(y_test, ridgereg_pred)
+print('ridgereg MAE: ' + str(sum(abs(ridgereg_pred - y_test))/(len(y_test))))
+print('ridgereg RMSE: ' + str(np.sqrt(ridgereg_rmse)))
+
 
 #-------------------------------decision tree train and test------------------------------------------------------------------
 
@@ -82,6 +108,24 @@ plt.show()
 
 #calculates feature importances that will be used in the custom kernel for SVM 3
 importances = randomforestmodel.feature_importances_
+
+
+
+
+
+#-------------------------------add gridsearch to the random forest (n_estimators, max_depth)----------------------------------
+gridsearch = GridSearchCV(estimator=rfr(),
+                          param_grid={
+                              'n_estimators':[100,200,300,400,500],
+                              'max_depth':[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+                              },
+                          scoring='neg_mean_squared_error')
+
+grid_result = gridsearch.fit(X_train, y_train)
+best_params = grid_result.best_params_
+print(best_params)
+
+
 
 #-------------------------------support vector machine 1 train and test------------------------------------------------------------
 
@@ -124,8 +168,6 @@ print('svr3 MAE: ' + str(sum(abs(svr3_pred - y_test))/(len(y_test))))
 print('svr3 RMSE: ' + str(np.sqrt(svr3_rmse)))
 
 
-
-
 #--------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------------------
@@ -133,11 +175,12 @@ print('svr3 RMSE: ' + str(np.sqrt(svr3_rmse)))
 #This section trains only the random forest model with the entire dataset 3
 #(which was determined to be the most accurate model in Phase 2), without
 #splitting it into a training and testing set. The RF model then makes
-#predictions based off of the parameters inputted into the predicted results csv.
+#predictions based off of the parameters input into the predicted results csv.
 
-
+'''
+#### Note that predicted results.csv is not the GitHub repository!
 literature_data = pd.read_csv('Feed In Data.csv')
-testvalues = pd.read_csv('predicted results.csv')
+#testvalues = pd.read_csv('predicted results.csv')
 
 #Dataset 3 - this line extracts dataset 3 from the complete dataset file
 literature_data = literature_data.drop(columns=['1author2', '1author3', 'LiTFSIwt3', 'temp C3', 'exponent3', 'Unnamed: 4', 'LiTFSIwt2', 'temp C2', 'exponent2', 'Unnamed: 9', '1author'])
@@ -153,3 +196,4 @@ randomforestmodel = rfr()
 randomforestmodel.fit(X_train, y_train)
 
 rf_pred = randomforestmodel.predict(testvalues)
+'''
